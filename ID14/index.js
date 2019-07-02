a110605326d61600009c4f9c51a9332e285b4f10
kska32 <kska32@gmail.com>
2019-07-03 08:00:05 +0900

    problem14

---
+++ ID14/index.js
@@ -0,0 +1,24 @@
+function solution(n){
+    function collatz(x=13){
+        let c = 1;
+        while(x>1){
+            x = x%2 ? 3*x+1 : x/2;
+            c++;
+        }
+        return c;
+    }
+    let max = 0;
+    let startNo = 0;
+    for(let i=1; i<n; i++){
+        let c = collatz(i);
+        if(max<c){
+            max = c;
+            startNo = i;
+        }
+    }
+    return [startNo,max];
+}
+
+console.time("start");
+    console.log(solution(1000000));
+console.timeEnd("start");
