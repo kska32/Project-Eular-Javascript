c0024e4e37e4446529567c6d8d2aa2e1345966ec
kska32 <kska32@gmail.com>
2019-07-11 10:41:33 +0900

    problem19

---
+++ ID19/index.js
@@ -0,0 +1,25 @@
+function solution(){
+    let isLeapYear = (x)=>x % 100 === 0 ? x % 400 === 0 : x % 4 ===0;
+    let m = (nthYear)=>{
+        let isLeap = isLeapYear(nthYear);
+        return {  
+            0:31, 1:isLeap?29:28, 2:31, 3:30, 4:31, 5:30, 
+            6:31, 7:31, 8:30, 9:31, 10:30, 11:31, len:isLeap?366:365
+        };
+    };
+    
+    let sundayCounts = 0;
+    let nthDays = 1 + 1 ; //1901.01.01 is Tuesday.
+    for(let i=1901; i<=2000; i++){
+        let r = m(i);
+        for(let h=0; h<12; h++){
+            nthDays += r[h];
+            if(nthDays%7===0) sundayCounts++;
+        }
+    }
+    return sundayCounts;
+}
+
+console.time("start");
+    console.log(solution());
+console.timeEnd("start");
