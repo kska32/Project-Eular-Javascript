0db4dc92c8947653bf37b606a89860c9579e64b5
kska32 <kska32@gmail.com>
2020-02-16 11:51:29 +0900

    .

--- ID20/index.js
+++ ID20/index.js
@@ -11,7 +11,7 @@ function solution(){
         }else{
             ret = math.bignumber(n);
         }  
-        return math.bignumber(ret); //123
+        return math.bignumber(ret); //1235
     }
 
 
