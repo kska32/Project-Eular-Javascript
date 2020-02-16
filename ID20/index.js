38d1307e415ee389e442b61dd4e38ac79755763a
sparrow32k <sparrow@firma-solutions.com>
2020-02-16 11:11:27 +0900

    problem20

---
+++ ID20/index.js
@@ -0,0 +1,34 @@
+math.config({
+  number: 'BigNumber',
+  precision: 1000   
+})
+
+function solution(){
+    let fac1 = (n)=>{
+        let ret; 
+        if( math.subtract(n,1) > 0){
+            ret = math.multiply(n, fac1(math.subtract(n, 1) ) );
+        }else{
+            ret = math.bignumber(n);
+        }  
+        return math.bignumber(ret);
+    }
+
+
+        let start = window.performance.now();
+        
+        let y = 0;
+        let ay = fac1(100).d;
+        ay.forEach((v)=>{
+            [...v.toString()].forEach((x)=>{
+                y += Number(x);
+            })
+        });
+        console.log('result:',y);
+        
+        let end = window.performance.now();
+        let time = end - start;
+        document.body.innerHTML = `The answer is ${y}, elapsed ${time}ms.`;
+}
+
+solution();
